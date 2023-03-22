88b49c8c78306b3b2df5acde38d974ee1c794c60
djlee <djlee@fxgear.net>
2023-03-22 23:13:48 +0900

    방에 입장하면 자기자신을 제외한 모든 유저한테 메시지 날리기

--- src/server.js
+++ src/server.js
@@ -21,6 +21,8 @@ wsServer.on("connection", socket => {
     socket.on("enter_room", (roomName, done) => {
         socket.join(roomName);
         done();
+
+        socket.to(roomName).emit("welcome");
     });
 })
 
