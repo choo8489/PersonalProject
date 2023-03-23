095b9721e686493cacedce89694e1642dc561147
djlee <djlee@fxgear.net>
2023-03-23 11:35:46 +0900

    Adapter 이용하여 소켓id와 룸의 정보를 가지고 룸이 몇개 생성되어잇는지 확인 하는 코드 작성

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -60,3 +60,17 @@ socket.on("bye", (left) => {
 })
 
 socket.on("new_message", addMessage);
+
+socket.on("room_change", (rooms) => {
+    console.log(rooms);
+    const roomList = welcome.querySelector("ul");
+    roomList.innerHTML = "";
+    if (rooms.length === 0) {
+        return;
+    }
+    rooms.forEach(room => {
+        const li = document.createElement("li");
+        li.innerText = room;
+        roomList.append(li);
+    });
+});
--- src/server.js
+++ src/server.js
@@ -17,17 +17,37 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wsServer = SocketIO(server);
 
+function publicRooms() {
+    const {sockets:{adapter:{sids, rooms}}} = wsServer;
+    const publicRooms = [];
+
+    rooms.forEach((_, key) => {
+        if (sids.get(key) === undefined){
+            publicRooms.push(key)
+        }
+    })
+
+    return publicRooms;
+}
+
 wsServer.on("connection", socket => {
     socket["nickname"] = "Anon";
+
     socket.on("enter_room", (roomName, done) => {
         socket.join(roomName);
         done();
         socket.to(roomName).emit("welcome", socket.nickname);
+        wsServer.sockets.emit("room_change", publicRooms());
     });
 
     socket.on("disconnecting", () => {
         socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nickname));
     });
+
+    socket.on("disconnect", () => {
+        wsServer.sockets.emit("room_change", publicRooms());
+    });
+
     socket.on("new_message", (msg, room, done) => {
         socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
         done();
