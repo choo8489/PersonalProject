d32994cec718d4c11485b724a71bd42c08f77a41
djlee@fxgear.net <djlee@fxgear.net>
2023-03-28 19:53:23 +0900

    Movie Module이랑 App모듈 분리   - 보통 AppModule에 모듈들을 올려서 사용
    app.GlobalPipes ValidationPipe 사용
    
    DTO 사용

--- hi-nest/src/app.module.ts
+++ hi-nest/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
-  controllers: [MoviesController],
+  imports: [MoviesModule],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
--- hi-nest/src/movies/movies.controller.ts
+++ hi-nest/src/movies/movies.controller.ts
@@ -1,31 +1,40 @@
-import { Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
+    constructor(private readonly moviesService: MoviesService){}
 
     @Get()
-    getAll() {
-        return 'This will return all movies';
+    getAll() : Movie[] {
+        return this.moviesService.getAll();
+    }
+
+    @Get("search")
+    search(@Query("year") seachingYear: string) {
+        return `We are searching for a movie made after: ${seachingYear}`;
     }
 
     @Get("/:id")
-    getOne(@Param('id') id: string) {
-        return `This will return one movie with the id: ${id}`;
+    getOne(@Param('id') id: number) : Movie {
+        return this.moviesService.getOne(id);
     }
 
     @Post()
-    create() {
-        return 'This will create a movie';
+    create(@Body() movieData: CreateMovieDto) {
+        return this.moviesService.create(movieData);
     }
 
     @Delete("/:id")
-    remove(@Param('id') id: string) {
-        return `This will delete a movie with the id: ${id}`;
+    remove(@Param('id') id: number) {
+        return this.moviesService.deleteOne(id);
     }
 
     @Patch("/:id")
-    patch(@Param('id') id: string) {
-        return `This will patch a movie with the id: ${id}`;
+    patch(@Param('id') id: number, @Body() updateData: UpdateMovieDto) {
+        return this.moviesService.update(id, updateData);
     }
-
 }
