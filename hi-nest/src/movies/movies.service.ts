797c08dea3cca6539cd12856a80b3b362a3a10b7
djlee@fxgear.net <djlee@fxgear.net>
2023-03-29 20:06:45 +0900

    jest 유닛 테스트 진행

--- hi-nest/src/movies/movies.service.ts
+++ hi-nest/src/movies/movies.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()
